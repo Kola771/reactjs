a206e6e2aaaf985bfa80ebccf1370573376f7b4a
Kola771 <koladeaboudou@gmail.com>
2024-02-21 12:44:53 +0100

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../input.css';
+import '../output.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul class="flex">
+    <nav class="bg-sky-600">
+      <ul class="m-auto w-[70%] flex p-4 bg-sky-600 justify-between">
         <li>
-          <Link to="/">Accueil</Link>
+          <Link to="/" class="text-white text-[15px]">Accueil</Link>
         </li>
         <li>
-          <Link to="/posts">Publication</Link>
+          <Link to="/posts" class="text-white text-[15px]">Publication</Link>
         </li>
         <li>
-          <Link to="/savepost">Sauvegarde des publications</Link>
+          <Link to="/savepost" class="text-white text-[15px]">Sauvegarde des publications</Link>
         </li>
       </ul>
     </nav>
